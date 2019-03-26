bc0eafd1d64241acaffa8b1839ba8895719e2f28
Tiny <1540524015@qq.com>
2019-03-26 23:19:37 +0800

    Session Variable placeholder

--- linearequation.py
+++ linearequation.py
@@ -27,3 +27,42 @@ for step in range(201):
 	sess.run(train)
 	if step % 20 == 0:
 		print(step, sess.run(Weights), sess.run(biases))
+		
+#
+matrix1 = tf.constant([[3,1]]) #1行2列
+matrix2 = tf.constant([[2],[2]]) #2行1列
+product = tf.matmul(matrix1,matrix2) #matrix multiply np.dot(m1,m2)
+
+#session 需要手动close 
+print("Session test")
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+sess.close()
+#session 自动close
+with tf.Session() as sess:
+	result2 = sess.run(product)
+	print(result2)
+	
+#Variable 操作
+print("Variable test")
+state = tf.Variable(0, name='counter')#定义一个variable
+print(state.name)
+one = tf.constant(1) #定义一个常量
+new_value = tf.add(state,one) #相加
+update = tf.assign(state,new_value) #更新
+init = tf.initialize_all_variables()
+with tf.Session() as sess:
+	sess.run(init)
+	for _ in range(3):
+		sess.run(update)
+		print(sess.run(state))
+
+#placeholder 类似占位变量，run时传入变量
+print("placeholder test")
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+output = tf.multiply(input1,input2)
+with tf.Session() as sess:
+	print(sess.run(output, feed_dict={input1:[7.0],input2:[2.]}))
+	
